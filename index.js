fd73dbe7bae99ba1d48854fbd5f0078a1dae3b30
KASHIF-SHOUKAT-SE <149269224+KASHIF-SHOUKAT-SE@users.noreply.github.com>
2024-05-03 11:37:46 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+// first.innerHTML
+// "i am span"
+// first.innerHTML = "<i>i am itallic</i>"
+// "<i>i am itallic</i>"
+// first.outerHTML ="<div>this is the outer html</div>"
+// "<div>this is the outer html</div>" 
+// console.log(document.body.firstChild.data)
+// console.log(document.body.textContent)
+
+let a = first.getAttribute("class")
+console.log(a)
+console.log(first.hasAttribute("class"))
+console.log(first.hasAttribute("style"))
+let first = document.getElementById("first")
+fisrt.setAttribute("hidden","true")
+
